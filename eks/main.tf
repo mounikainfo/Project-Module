3b6732f023b75672385e29270a6fc8b8670ec1a4
mounika <mounika.gg77@gmail.com>
2023-09-30 02:50:03 +0530

    new changes

--- eks/main.tf
+++ eks/main.tf
@@ -61,15 +61,5 @@ resource "aws_eks_node_group" "eks_ng_public" {
 }
 
 
-# data "aws_partition" "current" {}
 
-# resource "aws_iam_openid_connect_provider" "oidc_provider" {
-#     client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
-#     thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
-#     url             = aws_eks_cluster.sta_cluster.identity[0].oidc[0].issuer
-# }
-
-# locals {
-#   aws_iam_oidc_connect_provider_extract_from_arn = element(split("oidc-provider/", "${aws_iam_openid_connect_provider.oidc_provider.arn}"), 1)
-# }
 
