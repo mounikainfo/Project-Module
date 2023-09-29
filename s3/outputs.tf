9f7392c9b49334e5627c879b126cbe33702a294d
mounika <mounika.gg77@gmail.com>
2023-09-29 16:21:19 +0530

    commit 5

--- s3/outputs.tf
+++ s3/outputs.tf
@@ -1,9 +1,9 @@
 # export the s3 bucket name
-output "env_file_bucket_name" {
-  value = var.env_file_bucket_name
+output "env_file_bucket_name1" {
+  value = var.env_file_bucket_name1
 }
 
 # export the environment file name
-output "env_file_name" {
-  value = var.env_file_name
+output "env_file_name1" {
+  value = var.env_file_name1
 }
