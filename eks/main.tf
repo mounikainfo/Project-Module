6eea113cfc42b7512d9618c7c8a4974a1c2ac0b2
mounika <mounika.gg77@gmail.com>
2023-09-29 18:53:45 +0530

    eks module created

---
+++ eks/main.tf
@@ -0,0 +1,54 @@
+resource "aws_iam_role" "eks_master_role" {
+  name = "eks-master-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_master_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_master_role.name
+}
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name = "eks-cluster"
+  role_arn = aws_iam_role.eks_master_role.arn
+  version  = "1.27"
+
+  # security_groups  = [var.app_server_security_group_id]
+  vpc_config {
+    subnet_ids  = [var.private_app_subnet_az1_id, var.private_app_subnet_az2_id]
+    endpoint_private_access = false
+    endpoint_public_access  = true
+    public_access_cidrs     = ["0.0.0.0/0"]
+  }
+
+kubernetes_network_config {
+    service_ipv4_cidr = "172.20.0.0/16"
+  }
+
+# Enable EKS Cluster Control Plane Logging
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks_AmazonEKSVPCResourceController,
+  ]
+}
