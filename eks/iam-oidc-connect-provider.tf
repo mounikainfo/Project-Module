dbd7ef1aa839c91e1a4494d5d1baf6836bbf6831
mounika <mounika.gg77@gmail.com>
2023-09-30 02:56:16 +0530

    new changes

--- eks/iam-oidc-connect-provider.tf
+++ eks/iam-oidc-connect-provider.tf
@@ -1,6 +1,6 @@
 data "aws_partition" "current" {}
 
-resource "aws_iam_openid_connect_provider" "oidc_provider" {
+resource "aws_iam_openid_connect_provider" "oidc_provider1" {
     client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
     thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
     url             = aws_eks_cluster.sta_cluster.identity[0].oidc[0].issuer
