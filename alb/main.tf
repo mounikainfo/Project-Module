1ceba301ccb7daf8f03c48017b6eb8072a693218
mounika <mounika.gg77@gmail.com>
2023-09-30 19:29:33 +0530

    new

--- alb/main.tf
+++ alb/main.tf
@@ -1,16 +1,16 @@
-# create application load balancer
-resource "aws_lb" "application_load_balancer" {
-  name                       = "${var.project_name}-${var.environment}-alb"
-  internal                   = false
-  load_balancer_type         = "application"
-  security_groups            = [var.alb_security_group_id]
-  subnets                    = [var.public_subnet_az1_id, var.public_subnet_az2_id]
-  enable_deletion_protection = false
+# # create application load balancer
+# resource "aws_lb" "application_load_balancer" {
+#   name                       = "${var.project_name}-${var.environment}-alb"
+#   internal                   = false
+#   load_balancer_type         = "application"
+#   security_groups            = [var.alb_security_group_id]
+#   subnets                    = [var.public_subnet_az1_id, var.public_subnet_az2_id]
+#   enable_deletion_protection = false
 
-  tags = {
-    Name = "${var.project_name}-${var.environment}-alb"
-  }
-}
+#   tags = {
+#     Name = "${var.project_name}-${var.environment}-alb"
+#   }
+# }
 
 # create target group
 resource "aws_lb_target_group" "alb_target_group" {
