05804749ef6a15606633c1cd79ea13d3cf7cacce
mounika <mounika.gg77@gmail.com>
2023-09-30 12:19:22 +0530

    new

--- eks/iam-oidc-connect-provider.tf
+++ eks/iam-oidc-connect-provider.tf
@@ -1,11 +1,11 @@
-data "aws_partition" "current" {}
+# data "aws_partition" "current" {}
 
-resource "aws_iam_openid_connect_provider" "oidc_provider1" {
-    client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
-    # client_id_list = "sts.amazonaws.com"
-    thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
-    url             = aws_eks_cluster.sta_cluster.identity[0].oidc[0].issuer
-}
+# resource "aws_iam_openid_connect_provider" "oidc_provider1" {
+#     client_id_list  = ["sts.${data.aws_partition.current.dns_suffix}"]
+#     # client_id_list = "sts.amazonaws.com"
+#     thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
+#     url             = aws_eks_cluster.sta_cluster.identity[0].oidc[0].issuer
+# }
 # output "aws_iam_openid_connect_provider_arn" {
 #   description = "AWS IAM Open ID Connect Provider ARN"
 #   value       = aws_iam_openid_connect_provider.oidc_provider.arn
