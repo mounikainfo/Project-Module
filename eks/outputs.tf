60c5071bd77e17990998a5d07eaccc297d03cb47
mounika <mounika.gg77@gmail.com>
2023-09-30 15:33:45 +0530

    new

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -52,7 +52,7 @@ output "thumb" {
 output "identity_all" {
   value = aws_eks_cluster.sta_cluster.identity
 }
-
+  
 output "aws_iam_openid_connect_provider_extract_from_arn" {
   description = "AWS IAM Open ID Connect Provider extract from ARN"
   value       = local.aws_iam_oidc_connect_provider_extract_from_arn
