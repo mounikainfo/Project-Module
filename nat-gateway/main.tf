d47186141db86bb63468433faaafca2af021e1da
mounika <mounika.gg77@gmail.com>
2023-10-14 15:16:02 +0530

    new changes

--- nat-gateway/main.tf
+++ nat-gateway/main.tf
@@ -23,7 +23,7 @@ resource "aws_nat_gateway" "nat_gateway_az1" {
 
   # to ensure proper ordering, it is recommended to add an explicit dependency
   # on the internet gateway for the vpc
-  depends_on = [var.internet_gateway]
+  # depends_on = [var.internet_gateway]
 }
 
 # create nat gateway in public subnet az2
@@ -37,7 +37,7 @@ resource "aws_nat_gateway" "nat_gateway_az2" {
 
   # to ensure proper ordering, it is recommended to add an explicit dependency
   # on the internet gateway for the vpc
-  depends_on = [var.internet_gateway]
+  # depends_on = [var.internet_gateway]
 }
 
 # create private route table az1 and add route through nat gateway az1
