d78a81365b92310cee22dcf98b2e32fbb2b6be21
mounika <mounika.gg77@gmail.com>
2023-10-14 13:32:03 +0530

    new changes

--- nat-gateway/main.tf
+++ nat-gateway/main.tf
@@ -1,16 +1,12 @@
 # allocate elastic ip. this eip will be used for the nat-gateway in the public subnet az1 
 resource "aws_eip" "eip1" {
-  vpc = true
-
   tags = {
     Name = "${var.project_name}-${var.environment}-eip1"
   }
 }
 
 # allocate elastic ip. this eip will be used for the nat-gateway in the public subnet az2
-resource "aws_eip" "eip2" {
-  vpc = true
-
+resource "aws_eip" "eip2" { 
   tags = {
     Name = "${var.project_name}-${var.environment}-eip2"
   }
